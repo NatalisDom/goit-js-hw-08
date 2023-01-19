3c313043b48ee7c2f612b68dde5bcca4b4519f2f
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-01-19 17:02:06 +0200

    commit
    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+    "beforeend",
+    galleryItems
+      .map(
+        ({ preview, original, description }) =>
+          `         
+            <a class="gallery__link"  href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                data-painting="${original}"
+                alt="${description}"
+              />
+            </a>
+          
+          `
+      )
+      .join("")
+);
+  
+// Simple-lightbox init.
+const options = { captionsData: 'alt', captionDelay: 450 };
+
+var lightbox = new SimpleLightbox('.gallery a', options);
